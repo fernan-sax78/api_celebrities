919af16f764a7c41136e0bab682060af25763234
Fernando <fernan.sax@hotmail.com>
2023-09-23 22:26:08 +0200

    fetch

--- src/functions/fetch.js
+++ src/functions/fetch.js
@@ -1,9 +1,9 @@
 import Empty from "@/components/Empty_mess/Empty";
 
+const api_ = process.env.NEXT_PUBLIC_NOT_API_CELEBRITIES;
+
 export  const fetchData = async (setListing , setLoading) => {
-      
-  const api_ = process.env.NEXT_PUBLIC_NOT_API_CELEBRITIES;
-     
+          
       const response = await fetch(api_);
       const json = await response.json();
       setListing(json);
